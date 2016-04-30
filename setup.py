eeaa97d93539447ea2520c96c817f5a3e9da7507
Mansour Behabadi <mansour@oxplot.com>
2016-04-30 18:48:43 +1000

    Add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name='tamekit',
+  version='1.0.0',
+  description='Code execution control',
+  author='Mansour Behabadi',
+  author_email='mansour@oxplot.com',
+  url='https://github.com/oxplot/tamekit',
+  py_modules=['tamekit'],
+  license='BSD',
+)
